59e57a5e8968194e1780597f0a0a4200ccc432f1
Antoni-Hp <uw3#pwo0ja>
2019-08-28 20:48:12 +0200

    Zadanie 2

--- zad_2.py
+++ zad_2.py
@@ -12,11 +12,18 @@ try:
     search = driver.find_element_by_name("string")
     search.send_keys("rower")
     search.send_keys(Keys.RETURN)
-    print("price first bicycle: ", driver.find_element_by_css_selector("div.bf8839e").text)
+    first_bicycle = driver.find_element_by_css_selector("div.bf8839e").text
+    number_all_auctions = driver.find_element_by_css_selector("div._lsy4e._1hs1x._1ue2y._1t9p2._1h7wt._15mod._1vryf._1yfhn._3db39_1ZtAT._7ccvy").text.split()[-2]
+    print("price first bicycle: ", first_bicycle)
     print( "number of auctions per page: ", len(driver.find_elements_by_css_selector("div.bf8839e")))
-    print("number of all auctions: ", driver.find_element_by_css_selector("div._lsy4e._1hs1x._1ue2y._1t9p2._1h7wt._15mod._1vryf._1yfhn._3db39_1ZtAT._7ccvy").text.split()[-2])
-    driver.close()
-    print("Test PASS")
+    print("number of all auctions: ", number_all_auctions)
+    prince_in_gr = first_bicycle.split()[0].split(',')
+    print((int(prince_in_gr[0]) * 100) + int(prince_in_gr[1]))
+    # driver.close()
+    if ((int(prince_in_gr[0]) * 100) + int(prince_in_gr[1])) > int(number_all_auctions):
+        print("Test PASS")
+    else:
+        print("Test FAILL")
 
 except NoSuchElementException:
     print("Brak elementu")
