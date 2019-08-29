e9a4c59fa1f39e0b68f8134328640193fc1aed54
Antoni-Hp <uw3#pwo0ja>
2019-08-29 18:14:46 +0200

    Zadanie 2

---
+++ main.py
@@ -0,0 +1,8 @@
+import plan_1
+
+score_1 = plan_1.Main().start()
+
+if score_1 is True:
+    print("test PASS")
+else:
+    print("test FAIL")
---
+++ service.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Service():
+    def __init__(self, website):
+        self.driver = webdriver.Chrome(executable_path = 'C:\TestFiles\chromedriver.exe')
+        self.driver.get(website)
+
+    def getTitle(self):
+        return self.driver.title
+
+    def getFindElementbyId(self, id):
+        return self.driver.find_element_by_id(id)
+
+    def getFindElementbySelector(self, selector):
+        return self.driver.find_element_by_css_selector(selector)
+
+    def getFindElementbyName(self, name):
+        return self.driver.find_element_by_name(name)
+
+    def getFindElementsbyId(self, id):
+        return self.driver.find_elemens_by_id(id)
+
+    def getFindElementsbySelector(self, selector):
+        return self.driver.find_elements_by_css_selector(selector)
+
+    def getFindElementsbyName(self, name):
+        return self.driver.find_elements_by_name(name)
+
+    def getAkcept(self, value):
+        return value.send_keys(Keys.RETURN)
